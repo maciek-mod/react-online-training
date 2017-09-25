cd80abaa8fd2db39a0f60e3ca1af185cc2b32c6a
Maciej Modliński <maciej.modlinski@veneo.eu>
2017-09-25 13:38:23 +0200

    zadanie 2

--- src/index.js
+++ src/index.js
@@ -3,18 +3,24 @@ import ReactDOM from 'react-dom';
 
 import events from './data/events.json';
 
+
 const element = (
   <div>
     {events.map(item => {
-      return (
-          <li key={item.name}>{item.name} <br/>
-            <strong>{item.date}</strong> <br/>
-            {item.place}
-          </li>
-      );
+        const date = new Date(item.date);
+        if (date >= Date.now()) {
+          return (
+                  <li key={item.name}>{item.name} <br/>
+                    <strong>Data - {item.date}</strong> <br/>
+                    Miejsce - {item.place}
+                  </li>
+          );
+      }
+      return null;
+
     })}
-  </div>
+    </div>
 );
-// console.log(events);
+console.log(Date.now());
 
 ReactDOM.render(<ul>{element}</ul>, document.getElementById('root'));
