d5491c0feada9204b0d04417a71d8094d700bc60
maciek-mod <maciek.modlinski1988@gmail.com>
2017-09-26 20:49:06 +0200

    zadanie 6

--- src/Events.js
+++ src/Events.js
@@ -1,22 +1,45 @@
 import React from 'react';
 
-const Events = (props) => {
-    return(
-        <ul className="first">
-            {props.events.map(item => {
-                const date = new Date(item.date);
-                if (date >= Date.now()) {
-                  return (
-                          <li key={item.name}>{item.name} <br/>
-                            <strong>Data - {item.date}</strong> <br/>
-                            Miejsce - {item.place}
-                          </li>
-                  );
-              }
-              return null;
-            })}
-        </ul>
-    );
+// const Events = (props) => {
+//     return(
+//         <ul className="first">
+//             {props.events.map(item => {
+//                 const date = new Date(item.date);
+//                 if (date >= Date.now()) {
+//                   return (
+//                           <li key={item.name}>{item.name} <br/>
+//                             <strong>Data - {item.date}</strong> <br/>
+//                             Miejsce - {item.place}
+//                           </li>
+//                   );
+//               }
+//               return null;
+//             })}
+//         </ul>
+//     );
+// }
+
+
+class Events extends React.Component {
+  render() {
+      
+    return <ul className="first">
+        {this.props.events.map(item => {
+            const date = new Date(item.date);
+            if (date >= Date.now()) {
+              return (
+                      <li key={item.name}>{item.name} <br/>
+                        <strong>Data - {item.date}</strong> <br/>
+                        Miejsce - {item.place}
+                      </li>
+              );
+          }
+          return null;
+        })}
+    </ul>
+
+  }
 }
+
 
 export {Events};
