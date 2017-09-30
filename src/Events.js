bd6e41dc75f3352febf04460bd571902f04b232f
maciek-mod <maciek.modlinski1988@gmail.com>
2017-09-30 15:51:09 +0200

    poprawka

--- src/Events.js
+++ src/Events.js
@@ -28,24 +28,28 @@ class Events extends React.Component {
 
   render() {
 
-    return <ul className="first">
-        {this.props.events.map(item => {
-            const date = new Date(item.date);
-            if (date >= Date.now()) {
-              return (
-                      <li key={item.name}>{item.name} <br/>
-                        <strong>Data - {item.date}</strong> <br/>
-                        Miejsce - {item.place}
-                      </li>
-              );
-          }
-          return null;
-        })}
-    </ul>
+    return (
+        <ul>
+            {this.props.events.map(item => {
+                const date = new Date(item.date);
+                if (date >= Date.now()) {
+                  return (
+                          <li key={item.id}>
+                          <strong>{item.name}</strong>  <br/>
+                            Data - {item.date}<br/>
+                            Miejsce - {item.place}
+                          </li>
+                  );
+              }
+              return null;
+            })}
+        </ul>
+    );
 
   }
 
 }
+
 
 
 export {Events};
